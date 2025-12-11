982415d8587841dfb6a038604e85219a31f91fe5
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-12-11 03:48:51 +0330

    Update server.js

--- server.js
+++ server.js
@@ -7,29 +7,28 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"], 
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
+    cors: {
+        origin: ["http://localhost:3000", "http://localhost:5173"], 
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
 });
 
 io.on("connection", (socket) => {
-  console.log(" New client connected:", socket.id);
+    console.log(" New client connected:", socket.id);
 
-  socket.on("chat message", (msg) => {
-    const formatted = typeof msg === "string"
-      ? { user: { role: "admin" }, text: msg, time: new Date().toLocaleTimeString() }
-      : msg;
-  
+    socket.on("chat message", (msg) => {
+    const formatted = typeof msg === "string"? 
+    { user: { role: "admin" }, text: msg, time: new Date().toLocaleTimeString() }
+    : msg;  
     io.emit("chat message", formatted);
-  });
+    });
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnected:", socket.id);
-  });
+    socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id);
+    });
 });
 
 server.listen(3001, () => {
-  console.log("Server running on port 3001");
+    console.log("Server running on port 3001");
 });

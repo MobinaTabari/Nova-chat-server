40e0384931d351c2349669901778bb7dc182d0c9
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-12-11 07:02:31 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,14 +16,43 @@ const io = new Server(server, {
     },
 });
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error(" MongoDB connection error:", err));
+
+
+const MessageSchema = new mongoose.Schema({
+  user: {
+    role: String,
+  },
+  text: String,
+  time: String,
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+// API برای گرفتن پیام‌های ذخیره‌شده
+app.get("/messages", async (req, res) => {
+  const messages = await Message.find({});
+  res.json(messages);
+});
+
 io.on("connection", (socket) => {
     console.log(" New client connected:", socket.id);
 
-    socket.on("chat message", (msg) => {
+    socket.on("chat message", async(msg) => {
     const formatted = typeof msg === "string"? 
     { user: { role: "admin" }, text: msg, time: new Date().toLocaleTimeString() }
     : msg;  
-    io.emit("chat message", formatted);
+    try{
+        const saved = await Message.create(formatted);
+        io.emit("chat message", saved);
+    } catch(err){
+        console.log("Error saving :", err)
+    }    
     });
 
     socket.on("disconnect", () => {
@@ -29,6 +60,7 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
     console.log("Server running on port 3001");
 });
